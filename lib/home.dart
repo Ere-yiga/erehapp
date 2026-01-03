dab5e93adf25c19991d1081f4be2b3fd7e282935
Ere-yiga <erehdagr8@gmail.com>
2026-01-03 19:59:33 +0100

    lil bio completed

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,33 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ereyiga"),
+        title: Text(
+          "Ereyiga",
+          style: TextStyle(
+            color: Colors.white,
+          )
+          ),
         backgroundColor: Colors.brown,
         centerTitle: true,
       ),
-      body: Text("Hello there, you are welcome to my lil bio app"),
+      body: Column(
+        crossAxisAlignment:CrossAxisAlignment.start,
+        
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.asset("assets/ereyiga.png", height: 200,),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text("Hello there, I am Ereyiga, and you're welcome to my lil bio app."),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text("I am growing mobile app developer, Dart & Flutter")
+          )
+        ]       
+      )
     );
   }
-}
+}         
