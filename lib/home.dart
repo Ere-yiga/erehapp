250f775ba2bed3b19d68e3380ebc2b476028cac4
Ere-yiga <erehdagr8@gmail.com>
2026-01-10 02:39:32 +0100

    added more content, centerd them and made my inage circular

--- lib/home.dart
+++ lib/home.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Ereyiga",
-          style: TextStyle(
-            color: Colors.white,
-          )
-          ),
-        backgroundColor: Colors.brown,
+        title: Text("Simple bio card"),
+        backgroundColor: Colors.blue[100],
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment:CrossAxisAlignment.start,
-        
-        children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            child: Image.asset("assets/ereyiga.png", height: 200,),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            child: Text("Hello there, I am Ereyiga, and you're welcome to my lil bio app."),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            child: Text("I am growing mobile app developer, Dart & Flutter")
-          )
-        ]       
-      )
+      body: Container(
+        padding: EdgeInsets.all(50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/ereyiga.png"),
+            ),
+            Text("Ereyiga", style: TextStyle(fontSize: 30)),
+            Text("Growing mobile app dev", style: TextStyle(color: Colors.blue[700], fontSize: 20)),
+            Text("I learn and create beautiful mobile app",  style: TextStyle(fontSize:18)),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('500+\nfollowers', style: TextStyle(fontSize: 15)),
+                SizedBox(width:20),
+                Text('400+\nfollowing', style: TextStyle(fontSize: 15)),
+                SizedBox(width:20),
+                Text('5+\nprojects', style: TextStyle(fontSize: 15)),
+
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
-}         
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:erehapp/home.dart';
+import 'home.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home()
+    home: Home(),
   ));
 }
